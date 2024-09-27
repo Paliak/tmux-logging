0ed0eeb449eb87f4463fb3a648889a7e9b6cf28e
Paliak <91493239+Paliak@users.noreply.github.com>
2024-09-27 05:32:32 +0200

    FIX(prompt): use variable to indicate recording instead of command

--- scripts/toggle_logging.sh
+++ scripts/toggle_logging.sh
@@ -10,13 +10,13 @@ start_pipe_pane() {
     local file=$(expand_tmux_format_path "${logging_full_filename}")
     "$CURRENT_DIR/start_logging.sh" "${file}"
     display_message "Started logging to ${logging_full_filename}"
-    tmux send-keys 'set_recording_prompt' Enter
+    tmux setenv RECORDING "%D{[%T %D]} "
 }
 
 stop_pipe_pane() {
     tmux pipe-pane
     display_message "Ended logging to $logging_full_filename"
-    tmux send-keys 'set_normal_prompt' Enter
+    tmux setenv -u RECORDING
 }
 
 # returns a string unique to current pane
